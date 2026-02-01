41efd7c73d695637c3e8da56ada3035bcae2e224
ubuntu <maartenvandekemp@hotmail.com>
2026-02-01 08:50:00 +0100

    added main page

---
+++ src/main/java/com/NoviBackend/WalletWatch/mainpage/MainPageController.java
@@ -0,0 +1,12 @@
+package com.NoviBackend.WalletWatch.mainpage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainPageController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Novi!";
+    }
+}
